e1f3cd763f6abbf54815ce1afb1baa4c1811915e
世木宏幸 <hioyukiseki@segihiroyukinoMacBook-puro.local>
2020-12-06 18:33:22 +0900

    バリデーションの実装

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -13,10 +13,18 @@
 					<v-text-field
 						label="YouTube動画ID"
 						v-model="movieUrl"
+            :error-messages="movieErrorMessages"
 						placeholder="動画URLの【v= 】の後に続く英数字"
 						style="width: 300px"
 					/>
-					{{ movieUrl }}
+					<v-text-field
+						label="コメント"
+						v-model="comment"
+						:rules="commentRules"
+						placeholder="動画紹介のための任意のコメント"
+						class="mb-5"
+						style="width: 500px"
+					/>
 				</v-form>
 			</v-container>
 		</v-card>
@@ -28,7 +36,34 @@
 		data () {
 			return {
 				movieUrl: "",
+				movieErrorMessages: [],
+				comment: "",
+				commentRules: [
+          value => !!value || '入力してください',
+          value => value.length <= 16 || '16文字以内で入力してください'
+        ],
 			}
-		}
+		},
+
+    watch: {
+      movieUrl: function (value) {
+        const stringValidation = /^[0-9a-zA-Z]*$/.test(value)
+        if (!value) {
+          this.movieErrorMessages = [
+            '入力してください',
+          ]
+        } else if (value.length !== 11) { 
+          this.movieErrorMessages = [
+            '11文字で入力してください',
+          ]
+        } else if (!stringValidation) { 
+          this.movieErrorMessages = [
+            '半角英数記号で入力してください',
+          ]
+        } else {
+          this.movieErrorMessages = []
+        }
+      }
+    }
 	}
 </script>
