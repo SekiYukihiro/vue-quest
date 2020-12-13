05879d26d1f7eb317fe06d795cc068cf9dd7bace
SekiYukihiro <shiba920sof@yahoo.co.jp>
2020-12-13 15:02:20 +0900

    動画取得の実装

---
+++ src/components/Movies.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-row>
+    <v-col
+      v-for="item in movieInternalItems" :key="item.id"
+      cols="12"
+      sm="6"
+      md="4"
+      style="text-align: center"
+    >
+      <div>
+        <iframe
+          :src="item.url"
+          width="290"
+          height="163.125"
+          frameborder="0"
+        />
+        <p>{{ item.comment }}</p>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+	export default {
+    props: [
+      'movieItems',
+    ],
+
+    data () {
+      return {
+        movieInternalItems: {},
+      }
+    },
+
+    methods: {
+      // 動画の初期表示処理
+      init () {
+        this.movieInternalItems = []
+        Object.keys(this.movieItems[0]).forEach(i => {
+          console.log(this.movieItems[0][i])
+          const newItem = {
+            id: this.movieItems[0][i].url,
+            url: 'https://www.youtube.com/embed/' + this.movieItems[0][i].url + '?controls=1&loop=1&playlist=' + this.movieItems[0][i].url,
+            comment: this.movieItems[0][i].comment,
+          }
+          this.movieInternalItems.push(newItem)
+        })
+      },
+    },
+  }
+</script>
--- src/components/Top.vue
+++ src/components/Top.vue
@@ -5,26 +5,49 @@
       :formElevation="formElevation"
       @storeMovie="storeMovie"
     />
+    <movies
+      ref="movies"
+      :movie-items="movieItems"
+    />
   </v-container>
 </template>
 
 <script>
   import InputForm from './InputForm.vue'
+  import Movies from './Movies.vue'
+  import axios from 'axios'
+
 	export default {
     components: {
       InputForm,
+      Movies,
     },
 
     data () {
       return {
-      formElevation: "10",
+        formElevation: "10",
+        movieItems: {},
       }
     },
 
+    created () {
+      this.getMovies()
+    },
+
     methods: {
-      storeMovie (movieUrl, comment) {
-        console.log(movieUrl, comment) 
+      async getMovies () {
+        await axios.get('https://youtube-curation.herokuapp.com/rest/1'
+        ).then((response) => {
+          this.movieItems = [Object.assign({}, response.data.user.movies)]
+        }).catch((error) => {
+          console.log(error)
+        }).finally(() => {
+          this.$refs.movies.init() 
+        })
       },
+      storeMovie (movieUrl, comment) {
+        console.log(movieUrl, comment)
+      }
     },
   }
 </script>
